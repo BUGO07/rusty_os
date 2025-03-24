9459f77daba9baa344349b0f8fb78d7adfc56d26
BUGO07 <grigalagio07@gmail.com>
2025-03-25 02:03:12 +0400

    serial communication + unit tests

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -9,22 +9,15 @@ use rusty_os::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
-
     loop {}
 }
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
 
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     rusty_os::test_panic_handler(info)
